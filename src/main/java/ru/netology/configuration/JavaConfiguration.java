4ecada42e440e431c3a592385711b4993a3fe0f0
Michail Evseev <mixaevseev1989@gmail>
2023-05-29 21:08:13 +0300

    Initial commit

---
+++ src/main/java/ru/netology/configuration/JavaConfiguration.java
@@ -0,0 +1,23 @@
+package ru.netology.configuration;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ru.netology.systemProfile.DevProfile;
+import ru.netology.systemProfile.ProductionProfile;
+import ru.netology.systemProfile.SystemProfile;
+
+@Configuration
+public class JavaConfiguration {
+    @Bean
+    @ConditionalOnProperty(value = "netology.profile.dev", havingValue = "true")
+    public SystemProfile devProfile() {
+        return new DevProfile();
+    }
+
+    @Bean
+    @ConditionalOnProperty(value = "netology.profile.dev", havingValue = "false")
+    public SystemProfile prodProfile() {
+        return new ProductionProfile();
+    }
+}
